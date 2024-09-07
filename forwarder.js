42ddac304f2c3a15f55636824a4c12e9634ab0b1
Arman <astiwana@ualberta.ca>
2024-09-07 17:24:44 -0600

    added functionality for capturing messages in multiple channels

--- forwarder.js
+++ forwarder.js
@@ -10,9 +10,15 @@ dotenv.config();
 
 const client = new Client();
 
-const serverId = ''; 
-const targetChannelId = '';
-const destinationChannelId = '';
+const serverId = 'server_id'; // Server where messages are being captured
+
+// Map target channels to destination channels
+const channelMapping = {
+    'target_channel_1': 'destination_channel_1',  // Target Channel 1 -> Destination Channel 1
+    'target_channel_2': 'destination_channel_2',  // Target Channel 2 -> Destination Channel 2
+    'target_channel_3': 'destination_channel_3',  // Target Channel 3 -> Destination Channel 3
+    // Add more channels here if needed
+  };
 
 client.on('ready', () => {
   console.log(`🤖 Bot is online! Logged in as ${client.user.tag}`);
@@ -21,19 +27,26 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async (message) => {
-  if (message.guild && message.guild.id === serverId && message.channel.id === targetChannelId) {
-    const content = message.content;
-    const replyMessage = message.reference ? await message.channel.messages.fetch(message.reference.messageId) : null;
-    const replyContent = replyMessage ? `\n\n🔗 **In reply to ${replyMessage.author.username}:**\n${replyMessage.content}\n======` : '';
-
-    let messageContent = `🔔 **New Captured Message:**\n📬 **New Captured Message from ${message.author.username}**\n📨 **Message Content:**\n${content}${replyContent}`;
-
-    const messageLink = `🔗 **Message Link:** ${message.url}`;
-    messageContent += `\n${messageLink}\n=======`;
-
-    const destinationChannel = await client.channels.fetch(destinationChannelId);
-    destinationChannel.send(messageContent);
-  }
-});
-
-client.login("token");
+    if (message.guild && message.guild.id === serverId && channelMapping[message.channel.id]) {
+      const content = message.content;
+      const replyMessage = message.reference ? await message.channel.messages.fetch(message.reference.messageId) : null;
+      const replyContent = replyMessage ? `\n\n🔗 **In reply to ${replyMessage.author.username}:**\n${replyMessage.content}\n======` : '';
+  
+      let messageContent = `🔔 **New Captured Message:**\n📬 **New Captured Message from ${message.author.username}**\n📨 **Message Content:**\n${content}${replyContent}`;
+  
+      const messageLink = `🔗 **Message Link:** ${message.url}`;
+      messageContent += `\n${messageLink}\n=======`;
+  
+      // Fetch the destination channel based on the mapping
+      const destinationChannelId = channelMapping[message.channel.id];
+      const destinationChannel = await client.channels.fetch(destinationChannelId);
+      
+      if (destinationChannel) {
+        destinationChannel.send(messageContent);
+      } else {
+        console.error(`Destination channel not found for channel ID: ${message.channel.id}`);
+      }
+    }
+  });
+  
+  client.login("token");
